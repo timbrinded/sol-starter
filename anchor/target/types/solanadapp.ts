d7f833f5b4ce5716211a7f3b1d9b7daecde4f64c
Tim B <79199034+timbrinded@users.noreply.github.com>
2025-07-15 12:22:16 +0100

    ci fix

--- anchor/target/types/solanadapp.ts
+++ anchor/target/types/solanadapp.ts
@@ -5,162 +5,108 @@
  * IDL can be found at `target/idl/solanadapp.json`.
  */
 export type Solanadapp = {
-  "address": "4BTeVU7EXaDVCvYbZiWLgYinhiR4iJL7hhyRzwdP2au5",
-  "metadata": {
-    "name": "solanadapp",
-    "version": "0.1.0",
-    "spec": "0.1.0",
-    "description": "Created with Anchor"
-  },
-  "instructions": [
+  address: '4BTeVU7EXaDVCvYbZiWLgYinhiR4iJL7hhyRzwdP2au5'
+  metadata: {
+    name: 'solanadapp'
+    version: '0.1.0'
+    spec: '0.1.0'
+    description: 'Created with Anchor'
+  }
+  instructions: [
     {
-      "name": "close",
-      "discriminator": [
-        98,
-        165,
-        201,
-        177,
-        108,
-        65,
-        206,
-        96
-      ],
-      "accounts": [
+      name: 'close'
+      discriminator: [98, 165, 201, 177, 108, 65, 206, 96]
+      accounts: [
         {
-          "name": "payer",
-          "writable": true,
-          "signer": true
+          name: 'payer'
+          writable: true
+          signer: true
         },
         {
-          "name": "solanadapp",
-          "writable": true
-        }
-      ],
-      "args": []
-    },
-    {
-      "name": "decrement",
-      "discriminator": [
-        106,
-        227,
-        168,
-        59,
-        248,
-        27,
-        150,
-        101
-      ],
-      "accounts": [
-        {
-          "name": "solanadapp",
-          "writable": true
-        }
-      ],
-      "args": []
-    },
-    {
-      "name": "increment",
-      "discriminator": [
-        11,
-        18,
-        104,
-        9,
-        104,
-        174,
-        59,
-        33
-      ],
-      "accounts": [
-        {
-          "name": "solanadapp",
-          "writable": true
-        }
-      ],
-      "args": []
-    },
-    {
-      "name": "initialize",
-      "discriminator": [
-        175,
-        175,
-        109,
-        31,
-        13,
-        152,
-        155,
-        237
-      ],
-      "accounts": [
-        {
-          "name": "payer",
-          "writable": true,
-          "signer": true
+          name: 'solanadapp'
+          writable: true
         },
-        {
-          "name": "solanadapp",
-          "writable": true,
-          "signer": true
-        },
-        {
-          "name": "systemProgram",
-          "address": "11111111111111111111111111111111"
-        }
-      ],
-      "args": []
-    },
-    {
-      "name": "set",
-      "discriminator": [
-        198,
-        51,
-        53,
-        241,
-        116,
-        29,
-        126,
-        194
-      ],
-      "accounts": [
-        {
-          "name": "solanadapp",
-          "writable": true
-        }
-      ],
-      "args": [
-        {
-          "name": "value",
-          "type": "u8"
-        }
       ]
-    }
-  ],
-  "accounts": [
+      args: []
+    },
     {
-      "name": "solanadapp",
-      "discriminator": [
-        183,
-        218,
-        240,
-        102,
-        143,
-        38,
-        143,
-        116
+      name: 'decrement'
+      discriminator: [106, 227, 168, 59, 248, 27, 150, 101]
+      accounts: [
+        {
+          name: 'solanadapp'
+          writable: true
+        },
       ]
-    }
-  ],
-  "types": [
+      args: []
+    },
     {
-      "name": "solanadapp",
-      "type": {
-        "kind": "struct",
-        "fields": [
+      name: 'increment'
+      discriminator: [11, 18, 104, 9, 104, 174, 59, 33]
+      accounts: [
+        {
+          name: 'solanadapp'
+          writable: true
+        },
+      ]
+      args: []
+    },
+    {
+      name: 'initialize'
+      discriminator: [175, 175, 109, 31, 13, 152, 155, 237]
+      accounts: [
+        {
+          name: 'payer'
+          writable: true
+          signer: true
+        },
+        {
+          name: 'solanadapp'
+          writable: true
+          signer: true
+        },
+        {
+          name: 'systemProgram'
+          address: '11111111111111111111111111111111'
+        },
+      ]
+      args: []
+    },
+    {
+      name: 'set'
+      discriminator: [198, 51, 53, 241, 116, 29, 126, 194]
+      accounts: [
+        {
+          name: 'solanadapp'
+          writable: true
+        },
+      ]
+      args: [
+        {
+          name: 'value'
+          type: 'u8'
+        },
+      ]
+    },
+  ]
+  accounts: [
+    {
+      name: 'solanadapp'
+      discriminator: [183, 218, 240, 102, 143, 38, 143, 116]
+    },
+  ]
+  types: [
+    {
+      name: 'solanadapp'
+      type: {
+        kind: 'struct'
+        fields: [
           {
-            "name": "count",
-            "type": "u8"
-          }
+            name: 'count'
+            type: 'u8'
+          },
         ]
       }
-    }
+    },
   ]
-};
+}
